15dd6c303d3a28d225586a1085dace4477544255
ZacharyAdams <zaekadams@gmail.com>
2014-06-23 17:31:11 -0400

    Reviews now between 1-5; validations

--- app/models/review.rb
+++ app/models/review.rb
@@ -1,6 +1,11 @@
 class Review < ActiveRecord::Base
   validates :rating, :body, :user_id, :food_truck_id, presence: true
   validates :user_id, uniqueness: { scope: :food_truck_id }
+  validates_numericality_of :rating, numericality: {
+    only_integer: true,
+    greater_than_or_equal_to: 1,
+    less_than_equal_to: 5
+  }
 
   belongs_to :food_truck
   belongs_to :user
--- db/migrate/20140623211309_create_user_trucks.rb
+++ db/migrate/20140623211309_create_user_trucks.rb
@@ -1,8 +1,8 @@
 class CreateUserTrucks < ActiveRecord::Migration
   def change
     create_table :user_trucks do |t|
-      t.integer :user_id
-      t.integer :food_truck_id
+      t.integer :user_id, null: false
+      t.integer :food_truck_id, null: false
 
       t.timestamps
     end
