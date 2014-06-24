8dcc5ebcc7b26d6b091a95bb1ac8472bd518c69a
ZacharyAdams <zaekadams@gmail.com>
2014-06-24 11:41:16 -0400

    deleted unneeded files

--- app/models/user.rb
+++
@@ -1,4 +0,0 @@
-class User < ActiveRecord::Base
-  has_many :food_trucks, through: :user_trucks
-  has_many :reviews
-end
--- app/models/user_truck.rb
+++
@@ -1,6 +0,0 @@
-class UserTruck < ActiveRecord::Base
-  validates :user, presence: true
-  validates :food_truck, presence: true
-  belongs_to :food_truck
-  belongs_to :user
-end
