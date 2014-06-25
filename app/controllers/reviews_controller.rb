4b70f26412a14cdf0dc39a328b3a9b1149e4ed53
ZacharyAdams <zaekadams@gmail.com>
2014-06-25 13:27:42 -0400

    fixed hound errors

--- app/controllers/reviews_controller.rb
+++ app/controllers/reviews_controller.rb
@@ -29,5 +29,4 @@ class ReviewsController < ApplicationController
   def review_params
     params.require(:review).permit(:rating, :body)
   end
-
 end
--- spec/factories/reviews.rb
+++ spec/factories/reviews.rb
@@ -3,6 +3,7 @@ FactoryGirl.define do
     user
     food_truck
     rating 5
-    body 'This is the body and it must be 50 characters long. I like this food truck. DogeDogeDogeDogeDogeDogeDogeDogeDogeDogeDogeDoge'
+    body 'This is the body and it must be 50 characters long. I like this food truck.
+      DogeDogeDogeDogeDogeDogeDogeDogeDogeDogeDogeDoge'
   end
 end
