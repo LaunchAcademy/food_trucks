b162ca02302cac5e48d57f8a04d927522c419d29
sskates1 <simonirv2@gmail.com>
2014-06-25 17:21:35 -0400

    changed from for loop to FactoryGirl create_list for pagination testing

--- spec/features/add_pagination_spec.rb
+++ spec/features/add_pagination_spec.rb
@@ -8,11 +8,7 @@ feature 'user views food truck index page', %Q{
 } do
 
   scenario 'user can view multiple pages of trucks' do
-    25.times do |truck_number|
-      food_truck = FoodTruck.create!(name: "Dumpling#{truck_number}",
-        description: "This is at least a fifty-character description of a food truck.#{truck_number}",
-        category: 'Dumplings')
-    end
+    created_trucks = FactoryGirl.create_list(:food_truck, 11)
 
     visit food_trucks_path
     expect(page).to have_link 'Next ›'
