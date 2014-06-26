9697d2d39afe9fece5d4709d4ed7fcf406bd0e7d
RSid <sm1th303@gmail.com>
2014-06-26 18:30:22 -0400

    formatting validation for hound

--- app/models/review.rb
+++ app/models/review.rb
@@ -3,7 +3,8 @@ class Review < ActiveRecord::Base
   validates :user, presence: true
   validates :food_truck, presence: true
   validates :user, uniqueness: { scope: :food_truck }
-  validates :rating, numericality: {only_integer: true }, inclusion: { in: 1..5 }
+  validates :rating, numericality: { only_integer: true },
+    inclusion: { in: 1..5 }
 
   belongs_to :food_truck
   belongs_to :user
