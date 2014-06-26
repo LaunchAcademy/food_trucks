1f3e0b61fc7d50f818321bc0ddad9fa499be6755
RSid <sm1th303@gmail.com>
2014-06-26 18:29:23 -0400

    updates numericality validation syntax

--- app/models/review.rb
+++ app/models/review.rb
@@ -3,7 +3,7 @@ class Review < ActiveRecord::Base
   validates :user, presence: true
   validates :food_truck, presence: true
   validates :user, uniqueness: { scope: :food_truck }
-  validates_numericality_of :rating, inclusion: { in: 1..5 }
+  validates :rating, numericality: {only_integer: true }, inclusion: { in: 1..5 }
 
   belongs_to :food_truck
   belongs_to :user
