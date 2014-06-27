869f02056fce512e4d2877d199aa18056099be45
RSid <sm1th303@gmail.com>
2014-06-27 09:42:47 -0400

    Moved user is admin check into controller

--- app/models/user.rb
+++ app/models/user.rb
@@ -6,4 +6,8 @@ class User < ActiveRecord::Base
 
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable
+
+  def is_admin?
+    role == 'admin'
+  end
 end
