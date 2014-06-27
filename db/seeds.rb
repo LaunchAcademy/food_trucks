d1906a2cbfeb45b4880c0c9fc5eb10e5f807ee87
RSid <sm1th303@gmail.com>
2014-06-27 17:01:24 -0400

    More hound

--- db/seeds.rb
+++ db/seeds.rb
@@ -11,7 +11,8 @@ admin_user = User.create(email: 'admin@admin.com',
   password: 'admin123456',role: 'admin')
 
 food_truck = FoodTruck.create(name: 'stokes1',
-  description: 'This is at least a fifty-character description of a food truck.1',
+  description: 'This is at least a fifty-character description' +
+  'of a food truck.1',
   category: 'Pizza1')
 
 review = Review.create(user: user.id, food_truck: food_truck, rating: 4,
@@ -19,6 +20,7 @@ review = Review.create(user: user.id, food_truck: food_truck, rating: 4,
 
 20.times do |truck_number|
   food_truck = FoodTruck.create(name: "Dumpling#{truck_number}",
-    description: "This is at least a fifty-character description of a food truck.#{truck_number}",
+    description: 'This is at least a fifty-character description' +
+    "of a food truck.#{truck_number}",
     category: 'Dumplings')
 end
