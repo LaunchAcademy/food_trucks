1c12a24edb9c17cbb28e32067025061fee4ebe06
sskates1 <simonirv2@gmail.com>
2014-06-27 16:04:34 -0400

    Seperated files for editing and destroying trucks

--- spec/features/add_remove_trucks_spec.rb
+++ spec/features/add_remove_trucks_spec.rb
@@ -3,8 +3,6 @@ require 'rails_helper'
 feature 'user adds a new truck', %Q{
   As a user
   I want to add a new truck I found.
-  Edit trucks I mispelled
-  Delete trucks I have posted to keep it seceret.
 } do
 
   scenario 'user can add a food truck' do
@@ -25,7 +23,8 @@ feature 'user adds a new truck', %Q{
 
   scenario 'user can not add a food truck if they are not signed in' do
     visit new_food_truck_path
-    expect(page).to have_content 'You need to sign in or sign up before continuing.'
+    expect(page).to have_content 'You need to sign in or' +
+    ' sign up before continuing.'
   end
 
   scenario 'user tries to add a blank food truck' do
@@ -35,30 +34,6 @@ feature 'user adds a new truck', %Q{
     visit new_food_truck_path
     click_on 'Submit'
     expect(page).to have_content 'Uh oh! Your food truck could not be saved.'
-  end
-
-  scenario 'user tries to edit a food truck and leaves description blank' do
-    truck = FactoryGirl.create(:food_truck)
-    sign_in_as(truck.user)
-
-    visit food_truck_path(truck)
-    click_on 'Edit Food Truck'
-    fill_in 'Description', with: ''
-    click_on 'Save changes'
-    expect(page).to have_content 'Uh oh! Your Food Truck could not be updated.'
-    expect(page).to have_content 'Description can\'t be blank'
-  end
-
-  scenario 'user tries to edit a food truck with too short of a description' do
-    truck = FactoryGirl.create(:food_truck)
-    sign_in_as(truck.user)
-
-    visit food_truck_path(truck)
-    click_on 'Edit Food Truck'
-    fill_in 'Description', with: 'Short description here'
-    click_on 'Save changes'
-    expect(page).to have_content 'Uh oh! Your Food Truck could not be updated.'
-    expect(page).to have_content 'Description is too short (minimum is 50 characters)'
   end
 
   scenario 'user tries to add a food truck with no description' do
@@ -76,61 +51,7 @@ feature 'user adds a new truck', %Q{
 
     visit new_food_truck_path
     click_on 'Submit'
-    expect(page).to have_content 'Description is too short (minimum is 50 characters)'
-  end
-
-  scenario 'user can edit a truck they created' do
-    truck = FactoryGirl.create(:food_truck)
-
-    sign_in_as(truck.user)
-    visit food_truck_path(truck)
-
-    click_on 'Edit Food Truck'
-    fill_in 'Name', with: 'New Truck'
-    click_on 'Save changes'
-
-    expect(page).to have_content 'New Truck'
-  end
-
-  scenario 'user can remove a truck they created' do
-    truck = FactoryGirl.create(:food_truck)
-    sign_in_as(truck.user)
-
-    visit food_truck_path(truck)
-
-    click_on 'Delete Food Truck'
-
-    expect(page).to_not have_content truck.name
-  end
-
-  scenario 'user can not edit a truck they did not create' do
-    user = FactoryGirl.create(:user)
-    truck = FactoryGirl.create(:food_truck)
-
-    sign_in_as(user)
-    visit food_truck_path(truck)
-
-    expect(page).to_not have_button 'Edit Food truck'
-  end
-
-  scenario 'user can not go to an edit page that is not yours' do
-    user = FactoryGirl.create(:user)
-    truck = FactoryGirl.create(:food_truck)
-
-    sign_in_as(user)
-    visit edit_food_truck_path(truck)
-
-    expect(page).to have_content 'You did not post this food truck.'
-  end
-
-  scenario 'user can not remove a truck they did not create' do
-
-    user = FactoryGirl.create(:user)
-    truck = FactoryGirl.create(:food_truck)
-
-    sign_in_as(user)
-    visit food_truck_path(truck)
-
-    expect(page).to_not have_button 'Delete Food truck'
+    expect(page).to have_content 'Description is too short ' +
+    '(minimum is 50 characters)'
   end
 end
