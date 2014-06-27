eeb795a61c1cf1ca65ecd7886db42f41c5cb0882
sskates1 <simonirv2@gmail.com>
2014-06-27 11:21:42 -0400

    Fixed scope allowing edit and deletion.

--- app/controllers/food_trucks_controller.rb
+++ app/controllers/food_trucks_controller.rb
@@ -1,4 +1,6 @@
 class FoodTrucksController < ApplicationController
+  before_action :authenticate_user!, except: [:index, :show]
+
   def index
     @food_trucks = FoodTruck.order(created_at: :desc).page(params[:page])
   end
@@ -21,20 +23,17 @@ class FoodTrucksController < ApplicationController
   end
 
   def new
-    if !user_signed_in?
-      flash[:notice] = "You must be signed in to do that."
-      redirect_to '/food_trucks'
-    else
-      @food_truck = FoodTruck.new
-    end
+    @food_truck = FoodTruck.new
   end
 
   def edit
     @food_truck = FoodTruck.find(params[:id])
+    current_user_posted?(@food_truck)
   end
 
   def update
     @food_truck = FoodTruck.find(params[:id])
+    current_user_posted?(@food_truck)
     if @food_truck.update(food_truck_params)
       redirect_to(@food_truck)
     else
@@ -45,6 +44,7 @@ class FoodTrucksController < ApplicationController
 
   def destroy
     @food_truck = FoodTruck.find(params[:id])
+    current_user_posted?(@food_truck)
     if @food_truck.destroy
       flash[:notice] = "Your Food Truck was deleted"
       redirect_to '/'
@@ -54,6 +54,13 @@ class FoodTrucksController < ApplicationController
   end
 
   private
+
+  def current_user_posted?(food_truck)
+    if current_user != food_truck.user
+      flash[:notice] = 'You did not post this food truck.'
+      redirect_to food_trucks_path
+    end
+  end
 
   def food_truck_params
     params.require(:food_truck).permit(:name, :category, :description)
