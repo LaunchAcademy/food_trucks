66c7e218b231b39ebc18f88959a6e6dd1a8b2ee6
RSid <sm1th303@gmail.com>
2014-06-27 10:05:40 -0400

    Removing file that should be in separate branch

--- db/migrate/20140627014539_add_admin_to_users.rb
+++
@@ -1,9 +0,0 @@
-class AddAdminToUsers < ActiveRecord::Migration
-  def self.up
-    add_column :users, :role, :string, null: false, default: 'member'
-  end
-
-  def self.down
-    remove_column :users, :role
-  end
-end
