d7623afedfb64913253efd08cffb201d676cad70
Sam Gladstone <gladston@umich.edu>
2014-06-30 10:43:43 -0400

    Account for MailCatcher in test, production

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -41,5 +41,5 @@ Rails.application.configure do
   config.action_mailer.default_url_options = { host: 'localhost:3000' }
 
   config.action_mailer.delivery_method = :smtp
-  config.action_mailer.smtp_settings = { :address => "localhost", :port => 1025 }
+  config.action_mailer.smtp_settings = { address: 'localhost', port: 1025 }
 end
--- config/initializers/smtp.rb
+++ config/initializers/smtp.rb
@@ -1,7 +1,7 @@
-if Rails.env.development?
+if Rails.env.development? || Rails.env.test? || Rails.env.production?
   ActionMailer::Base.delivery_method = :smtp
   ActionMailer::Base.smtp_settings = {
-    address: "localhost",
+    address: 'localhost',
     port: 1025
   }
 end
