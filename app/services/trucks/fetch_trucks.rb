52252886bc6a8dd32a811a904013308f40029d16
RSid <sm1th303@gmail.com>
2014-07-01 11:51:21 -0400

    Refactored fetch all service object for readability

--- app/services/trucks/fetch_trucks.rb
+++ app/services/trucks/fetch_trucks.rb
@@ -1,16 +1,35 @@
+API_URL = 'http://data.streetfoodapp.com/1.1/schedule/boston/'
+
 class Trucks::FetchTrucks
 
   def self.call()
-    response = RestClient.get('http://data.streetfoodapp.com/1.1/schedule/boston/')
-    food_truck_data = JSON.parse(response)
-    parsable_food_truck_data = food_truck_data['vendors'].sort
-    parsable_food_truck_data.each do |food_truck_info|
-      unless FoodTruck.find_by(name: food_truck_info[1]['name'])
-        truck = FoodTruck.create(name: food_truck_info[1]['name'],category: 'Unknown',
-          description: food_truck_info[1]['description'],user_id: 1,
-          api_identifier: food_truck_info[1]['identifier'], twitter: '@' +
-          food_truck_info[1]['twitter'])
+
+    def import_food_truck(attributes)
+      FoodTruck.find_or_initialize_by(name: food_truck_info[1]['name']).tap do |truck|
+        truck.user_id = 1
+        truck.category = 'Unknown'
+        truck.description = food_truck_info[1]['description']
+        truck.api_identifier = food_truck_info[1]['identifier']
+
+        truck.save
       end
+    end
+
+    def import
+      food_trucks.each do |food_truck|
+        import_food_truck(food_truck)
+      end
+    end
+
+    def food_trucks
+      food_truck_data['vendors'].sort
+    end
+
+    private
+
+    def food_truck_data
+      response = RestClient.get(API_URL)
+      JSON.parse(response)
     end
   end
 end
