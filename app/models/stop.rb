ee17269feae62d66591e90ff83d0eaa6b0f258d3
RSid <sm1th303@gmail.com>
2014-07-02 14:03:32 -0400

    validating asociations, not foreign keys

--- app/models/stop.rb
+++ app/models/stop.rb
@@ -1,6 +1,6 @@
 class Stop < ActiveRecord::Base
-  validates :location_id, presence: true, numericality: { only_integer: true }
-  validates :food_truck_id, presence: true, numericality: { only_integer: true }
+  validates :location, presence: true
+  validates :food_truck, presence: true
   validates :time_arrive, presence: true, numericality: { only_integer: true }
 
   validates_uniqueness_of :time_arrive, scope: [:food_truck_id, :location_id]
